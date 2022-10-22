d1fb7fe498dc875d9c204772b32306149409646a
daviidco <pcordoba@cafetosoftware.com>
2022-10-22 17:33:34 -0500

    Migration initial db - Comment model producct

--- src/infrastructure/adapters/database/models/__init__.py
+++ src/infrastructure/adapters/database/models/__init__.py
@@ -11,7 +11,7 @@
 
 from src.infrastructure.adapters.database.models.user import User
 from src.infrastructure.adapters.database.models.company import Company
-from src.infrastructure.adapters.database.models.producto import *
+# from src.infrastructure.adapters.database.models.producto import *
 
 #
 # This file lets recognize the models to work with alembic.

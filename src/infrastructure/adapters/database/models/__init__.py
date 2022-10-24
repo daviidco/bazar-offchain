bbf88a8a542fc632ff35f916e5be67a38ff6f9f5
daviidco <pcordoba@cafetosoftware.com>
2022-10-24 18:22:07 -0500

    Modify structure to deploy EB-AWS - .flaskenv without schema

--- src/infrastructure/adapters/database/models/__init__.py
+++ src/infrastructure/adapters/database/models/__init__.py
@@ -10,7 +10,7 @@
 #
 
 from src.infrastructure.adapters.database.models.user import User
-from src.infrastructure.adapters.database.models.company import Company
+from src.infrastructure.adapters.database.models.company import *
 # from src.infrastructure.adapters.database.models.producto import *
 
 #

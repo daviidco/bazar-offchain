4cafabe8b2233092116c4578fd00ebf443948574
daviidco <pcordoba@cafetosoftware.com>
2022-10-24 13:48:33 -0500

    Modify structure to deploy EB-AWS

--- application.py
+++ application.py
@@ -8,18 +8,60 @@
 # 2022: Bazar Network S.A.S.
 # All Rights Reserved.
 #
-
 import os
 
-from src.infrastructure.adapters.flask import create_app
+from flask import Flask, jsonify
+from flask_restx import Api
 
-#
-# This file is the entrypoint of the application
-# @author David Córdoba
-#
+from src.infrastructure.adapters.flask.app.controllers.user.blueprints.user_blueprint_v1 import users_v1_01_bp
+from src.infrastructure.adapters.flask.app.controllers.company.blueprints.company_bp_v1_0_1 import companies_v1_01_bp
+from src.infrastructure.adapters.flask.app.controllers.avatar.blueprints.avatar_bp_v1_0_1 import avatars_v1_01_bp
+from src.infrastructure.adapters.flask.app.utils.logger import configure_logging
+from src.infrastructure.adapters.flask.configuration_injector import configure_inject
 
+
+application = Flask(__name__)
 settings_module = os.getenv('APP_SETTINGS_MODULE')
-app = create_app(settings_module)
+application.config.from_object(settings_module)
+# Load the configuration depending of environment
+config_env = application.config.get('ENV', 'LOCAL').lower()
+path_config_file = f'src/infrastructure/config/{config_env}.py'
+application.config.from_pyfile(f'{path_config_file}')
 
-if __name__ == "__main__":
-    app.run(port=8082)
+# Configure Logger
+configure_logging(application)
+application.logger.info(f'Environment: {config_env}')
+application.logger.info(f'Environment configuration file: {path_config_file}')
+
+configure_inject(application)
+
+# Catch errors 404
+Api(application,  catch_all_404s=True)
+
+# Disable strict mode when URL ends with /
+application.url_map.strict_slashes = False
+
+prefix = '/api'
+# Blueprints register
+application.register_blueprint(users_v1_01_bp, url_prefix=f'{prefix}')
+application.register_blueprint(companies_v1_01_bp, url_prefix=f'{prefix}')
+application.register_blueprint(avatars_v1_01_bp, url_prefix=f'{prefix}')
+application.logger.info('Blueprints Registered')
+
+@application.route(f'{prefix}/help', methods=['GET'])
+def help():
+    """Print available functions."""
+    routes = {}
+    rules = application.url_map.iter_rules()
+    for r in rules:
+        routes[r.rule] = {}
+        routes[r.rule]["functionName"] = r.endpoint
+        routes[r.rule]["methods"] = list(r.methods)
+
+    routes.pop("/static/<path:filename>")
+
+    return jsonify(routes)
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')

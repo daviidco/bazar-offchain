c1f275cf063ce7b2971d77ae4bcf53a244d4578c
daviidco <pcordoba@cafetosoftware.com>
2023-02-08 23:17:07 -0500

    feat: test implementation iterfaces

--- tests/company_test/company_interface_test.py
+++ tests/company_test/company_interface_test.py
@@ -1,13 +1,13 @@
 from unittest.mock import MagicMock
 
 from application import application
-from src.domain.ports.product_interface import IProductRepository
-from src.infrastructure.adapters.database.repositories.product_repository import ProductRepository
+from src.domain.ports.company_interface import ICompanyRepository
+from src.infrastructure.adapters.database.repositories.company_repository import CompanyRepository
 
 
-class TestProductRepository:
+class TestCompanyRepository:
 
-    def test_product_repository_implementation(self):
+    def test_company_repository_implementation(self):
         """
         Function to check all methods are implemented
         :return: Error when doesn't pass the test
@@ -15,16 +15,15 @@ class TestProductRepository:
         # Create an instance of the Repository class
         adapter_db = MagicMock()
         storage_repository = MagicMock()
-        utils_db = MagicMock()
-        repository = ProductRepository(adapter_db, storage_repository, utils_db)
+        repository = CompanyRepository(adapter_db, storage_repository)
         # Check that the instance implements the InterfaceRepository interface
-        assert isinstance(repository, IProductRepository)
+        assert isinstance(repository, ICompanyRepository)
         # Check that the instance has the required methods
         list_methods_repository = [func for func in dir(repository) if callable(getattr(repository, func))
                                    and not func.startswith("__")
                                    and not func.startswith("_")
                                    and not func == 'session_maker']
-        list_methods_interface = [func for func in dir(IProductRepository) if callable(getattr(IProductRepository, func))
+        list_methods_interface = [func for func in dir(ICompanyRepository) if callable(getattr(ICompanyRepository, func))
                                   and not func.startswith("__")]
 
         assert list_methods_repository == list_methods_interface
--- tests/wishlist_test/wishlist_interface_test.py
+++ tests/wishlist_test/wishlist_interface_test.py
@@ -1,30 +1,29 @@
 from unittest.mock import MagicMock
 
 from application import application
-from src.domain.ports.product_interface import IProductRepository
-from src.infrastructure.adapters.database.repositories.product_repository import ProductRepository
+from src.domain.ports.wishlist_interface import IWishListRepository
+from src.infrastructure.adapters.database.repositories.wishlist_repository import WishListRepository
 
 
-class TestProductRepository:
+class TestWishlistRepository:
 
-    def test_product_repository_implementation(self):
+    def test_wishlist_repository_implementation(self):
         """
         Function to check all methods are implemented
         :return: Error when doesn't pass the test
         """
         # Create an instance of the Repository class
         adapter_db = MagicMock()
-        storage_repository = MagicMock()
         utils_db = MagicMock()
-        repository = ProductRepository(adapter_db, storage_repository, utils_db)
+        repository = WishListRepository(adapter_db, utils_db)
         # Check that the instance implements the InterfaceRepository interface
-        assert isinstance(repository, IProductRepository)
+        assert isinstance(repository, IWishListRepository)
         # Check that the instance has the required methods
         list_methods_repository = [func for func in dir(repository) if callable(getattr(repository, func))
                                    and not func.startswith("__")
                                    and not func.startswith("_")
                                    and not func == 'session_maker']
-        list_methods_interface = [func for func in dir(IProductRepository) if callable(getattr(IProductRepository, func))
+        list_methods_interface = [func for func in dir(IWishListRepository) if callable(getattr(IWishListRepository, func))
                                   and not func.startswith("__")]
 
         assert list_methods_repository == list_methods_interface
